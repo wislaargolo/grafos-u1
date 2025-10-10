729e0a9c7795c6dc0dfec282458f58a11ab55236
Daniel Lucena <rlucena.daniel@gmail.com>
2025-10-10 18:02:41 -0300

    atualiza testes de q7 e q8

--- tests/q7.cpp
+++ tests/q7.cpp
@@ -7,24 +7,53 @@
 
 
 int main() {
-    std::cout << "Creating graph from file 'GRAFO_0.txt'...\n";
-    UndirectedAdjacencyListGraph<char> graph;
-    populate_graph_from_file("data/GRAFO_0.txt", graph);
-    std::cout << "Total quantity of nodes: " << graph.get_order() << std::endl;
+    std::cout << "Creating graph from file 'GRAFO_0.txt' with Undirected Adjacency List...\n";
+    UndirectedAdjacencyListGraph<char> UALgraph;
+    populate_graph_from_file("data/GRAFO_0.txt", UALgraph);
+    std::cout << "Total quantity of nodes: " << UALgraph.get_order() << std::endl;
+    std::cout << std::endl;
 
-    std::cout << "Creating graph from file 'GRAFO_1.txt'...\n";
-    UndirectedAdjacencyListGraph<char> graph1;
-    populate_graph_from_file("data/GRAFO_1.txt", graph1);
-    std::cout << "Total quantity of nodes: " << graph1.get_order() << std::endl;
+    std::cout << "Creating graph from file 'GRAFO_1.txt' with Undirected Adjacency Matrix...\n";
+    UndirectedAdjacencyMatrixGraph<char> UAMgraph;
+    populate_graph_from_file("data/GRAFO_1.txt", UAMgraph);
+    std::cout << "Total quantity of nodes: " << UAMgraph.get_order() << std::endl;
+    std::cout << std::endl;
 
-    std::cout << "Creating graph from file 'GRAFO_2.txt'...\n";
-    UndirectedAdjacencyListGraph<int> graph2;
-    populate_graph_from_file("data/GRAFO_2.txt", graph2);
-    std::cout << "Total quantity of nodes: " << graph2.get_order() << std::endl;
-    
-    std::cout << "Creating graph from file 'GRAFO_3.txt'...\n";
-    UndirectedAdjacencyListGraph<char> graph3;
-    populate_graph_from_file("data/GRAFO_3.txt", graph3);
-    std::cout << "Total quantity of nodes: " << graph3.get_order() << std::endl;
+    std::cout << "Creating graph from file 'GRAFO_2.txt' with Undirected Incidence Matrix...\n";
+    UndirectedIncidenceMatrixGraph<int> UIMgraph;
+    populate_graph_from_file("data/GRAFO_2.txt", UIMgraph);
+    std::cout << "Total quantity of nodes: " << UIMgraph.get_order() << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Creating graph from file 'GRAFO_3.txt' with Undirected Adjacency List...\n";
+    UndirectedAdjacencyListGraph<char> UALgraph2;
+    populate_graph_from_file("data/GRAFO_3.txt", UALgraph2);
+    std::cout << "Total quantity of nodes: " << UALgraph2.get_order() << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Creating graph from file 'DIGRAFO_0.txt' with Directed Adjacency List...\n";
+    DirectedAdjacencyListGraph<char> DALgraph;
+    populate_graph_from_file("data/DIGRAFO_0.txt", DALgraph);
+    std::cout << "Total quantity of nodes: " << DALgraph.get_order() << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Creating graph from file 'DIGRAFO_1.txt' with Directed Adjacency List...\n";
+    DirectedAdjacencyListGraph<int> DALgraph2;
+    populate_graph_from_file("data/DIGRAFO_1.txt", DALgraph2);
+    std::cout << "Total quantity of nodes: " << DALgraph2.get_order() << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Creating graph from file 'DIGRAFO_2.txt' with Directed Adjacency Matrix...\n";
+    DirectedAdjacencyMatrixGraph<int> DAMgraph;
+    populate_graph_from_file("data/DIGRAFO_2.txt", DAMgraph);
+    std::cout << "Total quantity of nodes: " << DAMgraph.get_order() << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Creating graph from file 'DIGRAFO_3.txt' with Directed Incidence Matrix...\n";
+    DirectedIncidenceMatrixGraph<char> DIMgraph;
+    populate_graph_from_file("data/DIGRAFO_3.txt", DIMgraph);
+    std::cout << "Total quantity of nodes: " << DIMgraph.get_order() << std::endl;
+    std::cout << std::endl;
+
     return 0;
 }
--- tests/q8.cpp
+++ tests/q8.cpp
@@ -15,6 +15,7 @@ int main() {
     UALGraph.print();
     //printa o tamanho do grafo
     std::cout << "Graph size: " << UALGraph.get_size() << std::endl;
+    std::cout << std::endl;
 
     //Tamanho do grafo usando matriz de adjacências
     //cria grafo usando matriz de adjacencia e printa o grafo
@@ -24,6 +25,17 @@ int main() {
     UAMGraph.print();
     //printa o tamanho do grafo
     std::cout << "Graph size: " << UAMGraph.get_size() << std::endl;
+    std::cout << std::endl;
+
+    //Tamanho do grafo usando matriz de incidência
+    //cria grafo usando matriz de incidencia e printa o grafo
+    std::cout << "Creating graph from file 'GRAFO_0.txt' to incidence matrix...\n";
+    UndirectedIncidenceMatrixGraph<char> UIMGraph;
+    populate_graph_from_file("data/GRAFO_0.txt", UIMGraph);
+    UIMGraph.print();
+    //printa o tamanho do grafo
+    std::cout << "Graph size: " << UIMGraph.get_size() << std::endl;
+    std::cout << std::endl;
 
     //Tamanho do digrafo usando lista de adjacências
     //cria digrafo usando lista de adjacencia e printa o digrafo
@@ -33,6 +45,7 @@ int main() {
     DALGraph.print();
     //printa o tamanho do digrafo
     std::cout << "Digraph size: " << DALGraph.get_size() << std::endl;
+    std::cout << std::endl;
 
     //Tamanho do digrafo usando matriz de adjacências
     //cria digrafo usando matriz de adjacencia e printa o digrafo
@@ -42,8 +55,18 @@ int main() {
     DAMGraph.print();
     //printa o tamanho do digrafo
     std::cout << "Digraph size: " << DAMGraph.get_size() << std::endl;
+    std::cout << std::endl;
 
-    
+    //Tamanho do digrafo usando matriz de incidência
+    //cria digrafo usando matriz de incidencia e printa o digrafo
+    std::cout << "Creating graph from file 'DIGRAFO_0.txt' to incidence matrix...\n";
+    DirectedIncidenceMatrixGraph<char> DIMGraph;
+    populate_graph_from_file("data/DIGRAFO_0.txt", DIMGraph);
+    DIMGraph.print();
+    //printa o tamanho do digrafo
+    std::cout << "Digraph size: " << DIMGraph.get_size() << std::endl;
+    std::cout << std::endl;
+
     return 0;
 }
 
